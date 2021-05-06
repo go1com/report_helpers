c4f613876538163594f0c4576a95ceb76119e327
Hong Truong <andytruong@users.noreply.github.com>
2021-05-06 10:30:10 +1000

    Update ExportCsv.php

--- ExportCsv.php
+++ ExportCsv.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace go1\report_helpers;
 
 use Elasticsearch\Client as ElasticsearchClient;
 use Elasticsearch\Common\Exceptions\Missing404Exception;
+use function is_null;
 
 class ExportCsv
 {
@@ -17,27 +19,19 @@ class ExportCsv
     }
 
     /**
-     * @param array $fields
-     * @param array $headers
-     * @param array $params
-     * @param array $selectedIds
-     * @param array $excludedIds
-     * @param bool $allSelected
-     * @param array $formatters
-     * @param array $customValuesSettings
      * @return resource
-     * @throws \RuntimeException
      */
     public function export(
-        $fields,
-        $headers,
-        $params,
-        $selectedIds,
-        $excludedIds,
-        $allSelected,
-        $formatters = [],
-        $customValuesSettings = [])
-    {
+        array $fields,
+        array $headers,
+        array $params,
+        array $selectedIds,
+        array $excludedIds,
+        bool $allSelected,
+        array $formatters = [],
+        array $customValuesSettings = [],
+        callable $preprocess = null,
+    ) {
         $stream = fopen('php://temp', 'w+b');
         if ($stream === false) {
             throw new \RuntimeException('Could not create file descriptor');
@@ -50,18 +44,19 @@ class ExportCsv
             // Improve performance by not loading all records then filter out.
             $params['body']['query']['bool']['must'][] = [
                 'ids' => [
-                    'values' => $selectedIds
-                ]
+                    'values' => $selectedIds,
+                ],
             ];
         }
 
-        $params += [
-            'scroll' => '30s',
-            'size' => 500,
-        ];
-
+        $params += ['scroll' => '30s', 'size' => 50];
 
         $docs = $this->elasticsearchClient->search($params);
+
+        if (!is_null($preprocess)) {
+            $preprocess($docs);
+        }
+
         $scrollId = $docs['_scroll_id'];
 
         while (true) {
@@ -73,17 +68,14 @@ class ExportCsv
                 foreach ($docs['hits']['hits'] as $hit) {
                     if (empty($excludedIds) || !in_array($hit['_id'], $excludedIds)) {
                         $csv = $this->getValues($fields, $hit, $formatters, $customValuesSettings);
-                        // Write row.
                         fputcsv($stream, $csv);
                     }
                 }
-            }
-            else {
+            } else {
                 if (isset($scrollId)) {
                     try {
                         $this->elasticsearchClient->clearScroll(['scroll_id' => $scrollId]);
-                    }
-                    catch (Missing404Exception $e) {
+                    } catch (Missing404Exception $e) {
                     }
                 }
                 break;
@@ -91,7 +83,7 @@ class ExportCsv
 
             $docs = $this->elasticsearchClient->scroll([
                 'scroll_id' => $scrollId,
-                'scroll'    => '30s'
+                'scroll'    => '30s',
             ]);
 
             if (isset($docs['_scroll_id'])) {
@@ -100,6 +92,7 @@ class ExportCsv
         }
 
         fseek($stream, 0);
+
         return $stream;
     }
 
@@ -114,15 +107,12 @@ class ExportCsv
                 } else {
                     $values[] = $formatters[$key]($hit);
                 }
-            }
-            else {
+            } else {
                 if (isset($formatters[$key]) && is_string($formatters[$key])) {
                     $value = array_get($hit['_source'], $formatters[$key]);
-                }
-                elseif (isset($hit['_source'][$key])) {
+                } elseif (isset($hit['_source'][$key])) {
                     $value = $hit['_source'][$key];
-                }
-                else {
+                } else {
                     $value = '';
                 }
                 if (is_array($value)) {
@@ -131,6 +121,7 @@ class ExportCsv
                 $values[] = $value;
             }
         }
+
         return $values;
     }
 
